424ce70bf7062f4006eb2271f32d039139b8be7f
Juan Carlos Ruiz <klaufel@gmail.com>
2022-06-24 20:44:27 +0200

    update companies script

--- scripts/generate-readme.js
+++ scripts/generate-readme.js
@@ -1,12 +1,16 @@
 const companies = require('../companies.json')
 const fs = require('fs')
 
-const REMOTE_WORK_ICONS = { NO: '🚫', YES: '✅' }
+const REMOTE_WORK_ICONS = {
+  FLEX: '🔄',
+  FULL: '✅',
+  NONE: '🚫',
+}
 
 const companiesMarkdown = companies.map(({ category, companies }, index) => {
   const header = `${!!index ? '\n' : ''}## ${category}\n| Nombre | Remoto | Stack | Dirección |\n| - | - | - | - |\n`
-  const body = companies.map(({ address, hasRemoteWork, name, stack, url }) =>
-    `| [${name}](${url}) | ${hasRemoteWork ? REMOTE_WORK_ICONS.YES : REMOTE_WORK_ICONS.NO} | ${stack || '-'} | ${address || '-'} |\n`
+  const body = companies.map(({ address, remoteWork, name, stack, url }) =>
+    `| [${name}](${url}) | ${REMOTE_WORK_ICONS[remoteWork] || '﹖'} | ${stack || '﹖'} | ${address || '﹖'} |\n`
   ).join('')
 
   return `${header}${body}`
@@ -17,16 +21,17 @@ const markdown = `
 
 > Listado de empresas con trabajos tecnológicos dentro de la Región de Murcia.
 
+Si conoces alguna más o puedes poner algún dato más sobre las que ya existen en el listado anímate a crear una PR!
+
 **Trabajo en remoto:**
 ﹖ No lo sabemos | 🚫 No | 🔄 Híbrido / flexible | ✅ 100% remoto
 
 ${companiesMarkdown}
-
-Si conoces alguna más o puedes poner algún dato más sobre las que ya existen en el listado anímate a crear una PR!`
+`
 
 try {
   fs.writeFileSync(`${process.cwd()}/README.md`, markdown, 'utf-8')
-  console.info('Markdown generated! 🍋')
+  console.log('\x1b[32m%s\x1b[0m', 'README generated successfully! 🍋\n')
 } catch (err) {
   console.error(err)
 }
